0b14cb4283fbb9b40bbcfe1e401ac758a298922c
AndreA <mail2andrewa@gmail.com>
2025-04-22 17:51:46 -0300

    add avante

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,4 +1,50 @@
 return {
-  -- 'github/copilot.vim',
-  'zbirenbaum/copilot.lua',
+  'github/copilot.vim',
+  -- 'zbirenbaum/copilot.lua',
+  -- cmd = 'Copilot',
+  -- build = ':Copilot auth',
+  -- event = 'InsertEnter',
+  -- config = function()
+  -- require('copilot').setup {
+  -- panel = {
+  -- enabled = true,
+  -- auto_refresh = false,
+  -- keymap = {
+  -- jump_prev = '[[',
+  -- jump_next = ']]',
+  -- accept = '<CR>',
+  -- refresh = 'gr',
+  -- open = '<M-CR>',
+  -- },
+  -- layout = {
+  -- position = 'bottom', -- | top | left | right
+  -- ratio = 0.4,
+  -- },
+  -- },
+  -- suggestion = {
+  -- enabled = true,
+  -- auto_trigger = true,
+  -- debounce = 75,
+  -- keymap = {
+  -- accept = '<M-l>',
+  -- next = '<M-]>',
+  -- prev = '<M-[>',
+  -- dismiss = '<C-]>',
+  -- },
+  -- },
+  -- filetypes = {
+  -- yaml = true,
+  -- markdown = true,
+  -- help = false,
+  -- gitcommit = true,
+  -- gitrebase = true,
+  -- hgcommit = false,
+  -- svn = false,
+  -- cvs = false,
+  -- ['*'] = true, -- enable for all filetypes
+  -- },
+  -- copilot_node_command = 'node', -- Node.js version must be >= 16.x
+  -- server_opts_overrides = {},
+  -- }
+  -- end,
 }
